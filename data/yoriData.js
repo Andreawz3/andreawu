87738b33ba328a5791a41b481051ad0d4b81ff30
Andrea Wu <andrea.wzg@gmail.com>
2024-02-11 16:12:08 -0800

    add info and style case study project 'Yori'

--- data/yoriData.js
+++ data/yoriData.js
@@ -6,31 +6,87 @@ export const yoriData = {
       websiteLink: "https://yori-nutritional-foods.vercel.app"
     }
   ],
-  features: [
+  overview: [
     {
-      title: "Cook Game",
-      image: "/images/yori/feature01.png",
-      description: "Embark on a culinary adventure with the captivating cook game. It offers a fun and educational experience for the little ones at home. Players select from a variety of ingredients to find the right ones and proceed to cook and create the meal"
-    },
-    {
-      title: "Meal Recipe",
-      image: "/images/yori/feature02.png",
-      description: "Discover recipes with detailed ingredient lists, step-by-step directions, and estimated preparation times. Elevate your cooking experience and savour every moment in the kitchen with our user-friendly recipe display feature"
-    },
-    {
-      title: "Shopping List",
-      image: "/images/yori/feature03.png",
-      description: "Smooth your grocery shopping experience with our convenient shopping list feature integrated seamlessly into the app. With just a tap, effortlessly generate a comprehensive shopping list from the ingredient lists of your selected recipes"
+      problem: "Malnutrition among children poses a significant threat to their overall development, affecting not only their physical health but also their cognitive abilities. When children lack essential nutrients due to inadequate diet or poor access to nutritious food, their growth may be stunted, leading to issues such as underweight or delayed development of muscles and bones",
+      solution: "The app has easy-to-follow recipes featuring nutritious ingredients. These recipes should be tailored to children's tastes and preferences,  on simplicity and affordability, and include step-by-step instructions. The app is accessible to children of all ages and abilities. Design an intuitive user interface with large, clear buttons and text for easy navigation"
     }
   ],
-  values: [
+  tool: [
     {
-      title: "Vision",
-      description: "To revolutionize the way families approach cooking and nutrition, fostering joyful and meaningful culinary experiences that unite generations and nourish both body and soul"
+      name: "Next.js",
+      image: "/images/tools/nextJS.png"
     },
     {
-      title: "Mission",
-      description: "Our mission is to empower families to create nutritive, delicious meals together, guided by our user-friendly platform that promotes health, affordability, and educational opportunities"
-    }
+      name: "GitHub",
+      image: "/images/tools/github.png"
+    },{
+      name: "Illustrator",
+      image: "/images/tools/illustrator.png"
+    },
+    {
+      name: "Figma",
+      image: "/images/tools/figma.png"
+    },
+    {
+      name: "Google Docs",
+      image: "/images/tools/docs.png"
+    },
   ],
+  skecthes: [
+    {
+      name: "Landing Page",
+      image: "/images/sketches/landing.png"
+    },
+    {
+      name: "Home Page",
+      image: "/images/sketches/home.png"
+    },
+    {
+      name: "Recipe Page",
+      image: "/images/sketches/recipe.png"
+    },
+    {
+      name: "Nutrition Page",
+      image: "/images/sketches/nutrition.png"
+    },
+    {
+      name: "Game Page",
+      image: "/images/sketches/game.png"
+    },
+  ],
+  color: [
+    {
+      name: "Tiffany Blue",
+      color: "#8EBDBD"
+    },
+    {
+      name: "Yellow Green",
+      color: "#A2BE52"
+    },
+    {
+      name: "Bittersweet",
+      color: "#E5645C"
+    },
+    {
+      name: "Earth Yellow",
+      color: "#E5A755"
+    },
+    {
+      name: "Saffron",
+      color: "#ECC64F"
+    },
+    {
+      name: "Bone",
+      color: "#E2D6C9"
+    },
+    {
+      name: "Green",
+      color: "#26594D"
+    },
+    {
+      name: "Avocado",
+      color: "#5A8C32"
+    },
+  ]
 }
--- pages/yori.js
+++ pages/yori.js
@@ -75,21 +75,124 @@ export default function Project() {
               height={70}
               alt="portfolio-image"
             />
-            <h1 className={styles.title}>MAIN FEATURES</h1>
+            <h1 className={styles.title}>PROJECT OVERVIEW</h1>
           </div>
-          <div className={styles.featuresContainer}>
+          { 
+            data && data.overview.map((d, index) => {
+              return (
+                <div key={index} style={{display: "flex"}} className={styles.overviewContainer}>
+                  <div className={styles.overviewInfo}>
+                    <p className={styles.overviewTitle}>Problem Statement</p>
+                    <p style={{textAlign: "justify"}}>{d.problem}</p>
+                  </div>
+                  <div className={styles.overviewInfo}>
+                    <p className={styles.overviewTitle}>Possible Solution</p>
+                    <p style={{textAlign: "justify"}}>{d.solution}</p>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+        <div className={styles.subContainer}>
+          <div className={styles.subContainerTitle}>
+            <Image 
+              src="/images/leaf-left.png"
+              width={70}
+              height={70}
+              alt="portfolio-image"
+            />
+            <h1 className={styles.title}>DESIGN PROCESS</h1>
+          </div>
+          <div className={styles.designProcessInfo}>
+            <Image 
+              src="/images/designProcess.png"
+              width={940}
+              height={165}
+              alt="design-process"
+            />
+            <div className={styles.designProcessText}>
+              <div style={{textAlign: 'center'}}>
+                <p style={{margin: "10px 0 5px 0"}}>Discover</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>User Research</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>Competitive Analysis</p>
+              </div>
+              <div style={{textAlign: 'center'}}>
+                <p style={{margin: "10px 0 5px 0"}}>Define</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>Survey</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>User Persona</p>
+              </div>
+              <div style={{textAlign: 'center'}}>
+                <p style={{margin: "10px 0 5px 0"}}>Ideate</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>Brainstorm Ideas</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>User Flow</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>Happy Path</p>
+              </div>
+              <div style={{textAlign: 'center'}}>
+                <p style={{margin: "10px 0 5px 0"}}>Design</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>Wireframes</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>Visual Design</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>Branding</p>
+              </div>
+              <div style={{textAlign: 'center'}}>
+                <p style={{margin: "10px 0 5px 0"}}>Test</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>Usability Test</p>
+                <p style={{fontSize: "var(--xsmall-font)", margin: 0}}>Implementing Feedback</p>
+              </div>
+            </div>
+            <div style={{display: "flex", gap: "250px", marginTop: "80px"}}>
+              <div>
+                <p style={{fontSize: "var(--medium-font)"}}>Project Duration</p>
+                <div style={{display: "flex", gap: "70px"}}>
+                  <p className={styles.projectDurationText}><span>10</span>weeks</p>
+                  <p className={styles.projectDurationText}><span>20+</span>screen</p>
+                </div>
+              </div>
+              <div>
+                <p style={{fontSize: "var(--medium-font)"}}>Tools Used</p>
+                <div style={{display: "flex", gap: "24px"}}>
+                  {
+                    data && data.tool.map((d, index) => {
+                      return (
+                        <div key={index} className={styles.toolsInfo}>
+                          <Image 
+                            src={d.image}
+                            width={70}
+                            height={70}
+                            alt={d.name}
+                          />
+                          <p>{d.name}</p>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.subContainer}>
+          <div className={styles.subContainerTitle}>
+            <Image 
+              src="/images/leaf-left.png"
+              width={70}
+              height={70}
+              alt="portfolio-image"
+            />
+            <h1 className={styles.title}>SKETCHES</h1>
+          </div>
+          <div className={styles.sketchesContainer}>
             {
-              data && data.features.map((d, index) => {
+              data && data.skecthes.map((d, i) => {
                 return (
-                  <div key={index} className={styles.featureDesc}>
+                  <div key={i}>
                     <Image 
                       src={d.image}
-                      width={230}
-                      height={500}
-                      alt={d.title}  
+                      width={115}
+                      height={250}
+                      alt={d.name}
                     />
-                    <p className={styles.featureTitle}>{d.title}</p>
-                    <p className={styles.featureDesc}>{d.description}</p>
+                    <p style={{textAlign: "center", fontSize: "var(--xsmall-font)"}}>{d.name}</p>
                   </div>
                 )
               })
@@ -104,27 +207,59 @@ export default function Project() {
               height={70}
               alt="portfolio-image"
             />
-            <h1 className={styles.title}>VALUES</h1>
+            <h1 className={styles.title}>STYLE GUIDE & BRANDING</h1>
           </div>
-          <div  className={styles.valuesContainer}>
-            {
-              data && data.values.map((d, index) => {
-                return (
-                  <div key={index} className={styles.valuesInfoContainer} style={{marginBottom: "130px"}}>
-                    <div className={styles.valuesTitleContainer}>
-                      <Image 
-                        src="/images/red-leaf.png"
-                        width={70}
-                        height={70}
-                        alt="red-leaf"
-                      />
-                      <p className={styles.valuesTitle}>{d.title}</p>
+          <div className={styles.brandingContainer}>
+            <div style={{marginBottom: "20px"}}>
+              <p className={styles.logo}>Logo</p>
+              <Image 
+                src="/images/yori/logo.png"
+                width={475}
+                height={148}
+                alt="portfolio-image"
+              />
+            </div>
+          </div>
+          <hr className={styles.horizontalLine}/>
+          <div  className={styles.colorContainer}>
+            <p className={styles.logo}>Color Palette</p>
+            <div className={styles.colorDisplay}>
+              {
+                data && data.color.map((d, i) => {
+                  return (
+                    <div key={i} className={styles.colorInfo}>
+                      <div className={styles.color} style={{backgroundColor:`${d.color}`}}></div>
+                      <p style={{margin: "10px 0 0 0", fontSize: "var(--xsmall-font)"}}>{d.color}</p>
+                      <p style={{margin: 0, fontSize: "var(--xsmall-font)"}}>{d.name}</p>
                     </div>
-                    <p className={styles.valuesDesc}>{d.description}</p>
-                  </div>
-                )
-              })
-            }
+                  )
+                })
+              }
+            </div>
+          </div>
+          <hr className={styles.horizontalLine}/>
+          <div  className={styles.typoContainer}>
+            <p className={styles.logo}>Typography</p>
+            <div style={{display: "flex", gap: "100px", marginBottom: "60px"}}>
+              <div>
+                <p className={styles.typoTitle}>Title Header</p>
+                <Image 
+                  src="/images/capuche.png"
+                  width={340}
+                  height={325}
+                  alt="portfolio-image"
+                />
+              </div>
+              <div>
+                <p className={styles.typoTitle}>Body Text</p>
+                <Image 
+                  src="/images/lato.png"
+                  width={340}
+                  height={325}
+                  alt="portfolio-image"
+                />
+              </div>
+            </div>
           </div>
         </div>
         <Footer />
